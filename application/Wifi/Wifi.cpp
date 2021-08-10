ab47e21a64b20e97135e5dcea8416b9e82c4a557
HS Grobbelaar <roughleaf.development@gmail.com>
2021-08-10 22:49:26 +0200

    Progressed on Wifi Driver

--- application/Wifi/Wifi.cpp
+++ application/Wifi/Wifi.cpp
@@ -5,28 +5,65 @@
 namespace WIFI
 {
     // Statics
-    char Wifi::mac_addr_cstr[]{};
-    std::mutex Wifi::first_call_mutx;
-    bool Wifi::first_call{true};
+    char                Wifi::mac_addr_cstr[]{};
+    std::mutex          Wifi::init_mutx;
+    Wifi::state_e       Wifi::_state{state_e::NOT_INITIALISED};
+    wifi_init_config_t  Wifi::wifi_init_cfg = WIFI_INIT_CONFIG_DEFAULT();
+    wifi_config_t       Wifi::wifi_cfg{};
 
     constexpr static const char *ssid{"TestGuest"};
     constexpr static const char *password{"00000000"};
 
+    // Wifi Contrustor
     Wifi::Wifi(void)
     {
-        std::lock_guard<std::mutex> guard(first_call_mutx);
-        if(first_call)
+        std::lock_guard<std::mutex> guard(init_mutx);
+        if(!mac_addr_cstr[0])
         {
-            if(ESP_OK != _get_mac()) esp_restart();
-            first_call = false;
+            if(ESP_OK != _get_mac())
+            {
+                esp_restart();
+            }
         }
-        _wifi_initialise();
+    }
+
+    esp_err_t Wifi::_init(void)
+    {
+        std::lock_guard<std::mutex> guard(init_mutx);
+
+        esp_err_t status{ESP_OK};
+
+        if(state_e::NOT_INITIALISED == _state)
+        {
+            status |= esp_netif_init();
+            if (ESP_OK == status)
+            {
+                const esp_netif_t* const p_netif = esp_netif_create_default_wifi_sta();
+
+                if(!p_netif)
+                {
+                    status = ESP_FAIL;
+                }
+            }
+
+            if(ESP_OK == status)
+            {
+                status = esp_wifi_init(&wifi_init_cfg);
+            }
+
+        }
+        else if (state_e::ERROR == _state)
+        {
+            status = ESP_FAIL;
+        }
+
+        return status;
     }
 
     // TODO Major refactor, move config to static classwide scope
     // Add error checking afer each stage.
     // Impliment the state machine
-    esp_err_t Wifi::_wifi_initialise()
+    esp_err_t Wifi::init(void)
     {
         esp_err_t status{ESP_OK};
         wifi_init_config_t _cfg = WIFI_INIT_CONFIG_DEFAULT();
@@ -53,6 +90,7 @@ namespace WIFI
         return status;
     }
 
+    // Get default MAC from API and convert to ASCII HEX
     esp_err_t Wifi::_get_mac(void)
     {
         uint8_t mac_byte_buffer[6]{};
--- application/Wifi/Wifi.h
+++ application/Wifi/Wifi.h
@@ -1,11 +1,18 @@
 #pragma once
 
-#include "esp_wifi.h"
-#include "esp_log.h"
-
+#include <cstring>
 #include <mutex>
 
-#include <cstring>
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+
+
+
 
 namespace WIFI
 {
@@ -28,24 +35,37 @@ namespace WIFI
             ERROR
         };
 
+        // "Rule of 5" Constructors and assignment operators
+        // Ref: https://en.cppreference.com/w/cpp/language/rule_of_three
         Wifi(void);
+        ~Wifi(void)                     = default;
+        Wifi(const Wifi&)               = default;
+        Wifi(Wifi&&)                    = default;
+        Wifi& operator=(const Wifi&)    = default;
+        Wifi& operator=(Wifi&&)         = default;
 
         esp_err_t init(void);  // Setup the stuff
         esp_err_t begin(void); // Start Wifi, connect, etc...
 
         state_e get_state(void);
-        const char *get_mac(void) { return mac_addr_cstr; }
+
+        constexpr static const char *get_mac(void) { return mac_addr_cstr; }
 
     private:
-        void state_machine(void);
+        static esp_err_t _init(void);
 
-        esp_err_t _get_mac(void);
-        esp_err_t _wifi_initialise(void);
+        static void state_machine(void);
+        static state_e _state;
+
+        // Get default MAC from API and convert to ASCII HEX
+        static esp_err_t _get_mac(void);
 
         static char mac_addr_cstr[13];
 
-        static std::mutex first_call_mutx;
-        static bool first_call;
+        static std::mutex init_mutx;
+
+        static wifi_init_config_t wifi_init_cfg;
+        static wifi_config_t wifi_cfg;
 
     }; // Wifi class
 
